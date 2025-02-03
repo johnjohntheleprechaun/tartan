a59f197d1fdcc08b1fc3078fbf9de59bbab751d6
John Jorgensen <john@leprechaun.dev>
2025-02-03 22:28:39 +0000

    update binary so it actually runs

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ npm install
 
 echo "building typescript..."
 npx tsc
+chmod +x dist/index.js
 
 echo "processing component library..."
 npx ts-node src/build.ts
--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import parse from "node-html-parser";
 import {readFileSync} from "fs";
 import {findCustomeTags, resolveCustomTag} from "./resolve";
