61634c27dbda1342ec26479ea254986f4124cad1
John Jorgensen <john@leprechaun.dev>
2025-02-04 02:56:09 +0000

    insert script that defines HTML elements

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import parse from "node-html-parser";
-import {readFileSync} from "fs";
+import {readFileSync, writeFileSync} from "fs";
 import {findCustomeTags, resolveCustomTag} from "./resolve";
 import {generateWebComponentJS} from "./code-gen";
 
@@ -10,4 +10,13 @@ const page = parse(textContent);
 
 const tags = findCustomeTags(page);
 
-(async () => {console.log(await generateWebComponentJS(Array.from(tags)));})();
+(async () => {
+    const code = await generateWebComponentJS(Array.from(tags));
+    const body = page.querySelector("body");
+    if (!body) {
+        throw new Error("document doesn't have a body");
+    }
+    body.insertAdjacentHTML("afterbegin", `<script>${code}</script>`);
+
+    writeFileSync("out.html", page.toString());
+})();
