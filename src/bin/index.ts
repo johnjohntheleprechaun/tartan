4e25038fde517bad1ea7aa90dcff3ae8514f3989
John Jorgensen <john@leprechaun.dev>
2025-02-15 17:41:37 +0000

    start using commander for options

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -3,11 +3,21 @@
 import {HTMLProcessor} from "./process.js";
 import fs from "fs/promises";
 import {TagNameResolver} from "./resolve.js";
+import {Command} from "@commander-js/extra-typings";
 
-(async () => {
-    const resolver: TagNameResolver = await TagNameResolver.create("./tartan.config.json");
-    const processor: HTMLProcessor = await HTMLProcessor.create("./test.html", resolver);
+const program = new Command();
 
-    const result = await processor.process();
-    await fs.writeFile("./out.html", result);
-})();
+program.version("0.0.1")
+program.command("build")
+    .argument("<entry>", "The HTML file to process")
+    .option("--config-file <path>", "The path to your tartan config file", "tartan.config.json")
+    .option("-o, --ouput <path>", "The name of the file to output", "out.html")
+    .action(async (entry, opts) => {
+        const resolver: TagNameResolver = await TagNameResolver.create(opts.configFile);
+        const processor: HTMLProcessor = await HTMLProcessor.create(entry, resolver);
+
+        const result = await processor.process();
+        await fs.writeFile(opts.ouput, result);
+    });
+
+program.parse();
