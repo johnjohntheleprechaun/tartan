efb5367d6f7c961a28b6a491579572776ae0cc78
John Jorgensen <john@leprechaun.dev>
2025-03-03 05:05:36 +0000

    shrink the tartan export definition

--- src/tartan-export.d.ts
+++ src/tartan-export.d.ts
@@ -1,17 +1,8 @@
 export interface TartanExport {
     defaultPrefix: string;
-    dynamicPrefixSupport: boolean;
 
     /**
      * A string mapping component names *without the prefix* to module imports.
      */
     componentMap: {[key: string]: string} | ((key: string) => string);
-
-    providerElements: ProviderElementDefinition[];
-}
-
-export interface ProviderElementDefinition {
-    elementName: string;
-
-    // other things like a list of events it listens for so that I can try to dyanmically avoid interference?
 }
