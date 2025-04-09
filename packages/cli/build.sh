02ab31ce88d1aab97ac754bbd53328e368a71f2f
John Jorgensen <john@leprechaun.dev>
2025-04-09 15:05:34 +0000

    update build scripts and package definitions to be actually correct lol

--- packages/cli/build.sh
+++ packages/cli/build.sh
@@ -1,6 +1,9 @@
 echo "ensuring packages are installed..."
 npm install
 
+echo "deleting previous build..."
+rm -r dist
+
 echo "building typescript..."
 npx tsc
-chmod +x dist/bin/index.js
+chmod +x dist/index.js
