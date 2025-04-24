2fd49d19df280e792b9338026742626f6619bb23
John Jorgensen <john@leprechaun.dev>
2025-04-23 23:02:55 -0600

    switch to using jasmine for tests because fuck you

--- packages/core/spec/logger.spec.ts
+++ packages/core/spec/logger.spec.ts
@@ -1,36 +1,35 @@
 import {Logger} from "../src/logger";
 
 describe("Test log level logic", () => {
-    let logSpy: jest.SpyInstance;
+    let logSpy: jasmine.Spy;
     beforeAll(() => {
-        logSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+        logSpy = spyOn(console, "log");
     });
     beforeEach(() => {
-        logSpy.mockClear();
+        logSpy.calls.reset();
         Logger.logLevel = Logger.defaultLogLevel;
     });
 
-    test("default log level shouldn't be called", () => {
+    it("default log level shouldn't be called", () => {
         Logger.log("adfasdf");
         expect(logSpy).not.toHaveBeenCalled();
     });
-
-    test("a message with the default verbosity should be logged when logLevel is set to 1", () => {
+    it("a message with the default verbosity should be logged when logLevel is set to 1", () => {
         Logger.logLevel = 1;
         Logger.log("adfasdf");
         expect(logSpy).toHaveBeenCalled();
     });
-
-    test("logging at level 0 should be logged with no changes to the logger", () => {
+    it("logging at level 0 should be logged with no changes to the logger", () => {
         Logger.log("this should be logged", 0);
         expect(logSpy).toHaveBeenCalled();
     });
-
-    test("a log level greater than or equal to the verbosity specified at call time should mean it's always logged", () => {
+    it("a log level greater than or equal to the verbosity specified at call time should mean it's always logged", () => {
         Logger.logLevel = 10;
         Logger.log("asdf", 3);
         Logger.log("asdf", 10);
         Logger.log("asdf", 5);
         Logger.log("asdf", 1);
-    })
-})
+
+        expect(logSpy).toHaveBeenCalledTimes(4);
+    });
+});
