cf55d7e4da561bf3b9012eededfb5b2fb902f8b2
John Jorgensen <john@leprechaun.dev>
2025-05-16 21:53:56 -0600

    remove the schema for tartan config, cause at no point am I actually using it lol

--- packages/core/src/tartan-config.ts
+++ packages/core/src/tartan-config.ts
@@ -1,61 +1,16 @@
-import {JSONSchema, FromSchema} from "json-schema-to-ts";
-import {TartanContextFile, tartanContextSchema} from "./tartan-context.js";
+import {TartanContextFile} from "./tartan-context.js";
 import {ReplaceTypes} from "./util.js";
 
-export const tartanConfigSchema = {
-    type: "object",
-    properties: {
-        rootDir: {
-            type: "string",
-            description: ""
-        },
-        outputDir: {
-            type: "string",
-        },
-        designLibraries: {
-            type: "array",
-            items: {
-                type: "string",
-            },
-        },
-        pathPrefixes: {
-            type: "object",
-            patternProperties: {
-                "*": {
-                    type: "string",
-                },
-            },
-        },
-        rootContext: {
-            ...tartanContextSchema,
-            allOf: [
-                {
-                    if: {
-                        properties: {pageMode: {const: "file"}},
-                    },
-                    then: {
-                        required: ["pageSource"],
-                    },
-                },
-                {
-                    if: {
-                        properties: {pageMode: {const: "directory"}},
-                    },
-                    then: {
-                        required: ["pagePattern"],
-                    },
-                },
-            ],
-        },
-    },
-    required: [
-        "rootDir",
-        "outputDir",
-    ],
-    additionalProperties: false,
-} as const satisfies JSONSchema;
+/**
+ * Requiring pagemode and the relevant properties to be set (but we don't need to export this)
+ */
+type FullTartanContext = ReplaceTypes<TartanContextFile, {pageMode: "file", pageSource?: string, pagePattern: string}>
+    | ReplaceTypes<TartanContextFile, {pageMode: "directory", pageSource: string}>;
 
-export type TartanConfig = ReplaceTypes<FromSchema<typeof tartanConfigSchema>, {
-    rootContext?: ReplaceTypes<TartanContextFile, {pageMode: "file", pagePattern: string}>
-    | ReplaceTypes<TartanContextFile, {pageMode: "directory", pageSource: string}>
-}>;
+export type TartanConfig = {
+    rootDir: string;
+    outputDir: string;
+    designLibraries?: string[];
+    pathPrefixes?: {[key: string]: string};
+    rootContext?: FullTartanContext
+};
