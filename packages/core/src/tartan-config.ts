731d9295619704c64a9ecdb118b45ae9fac192cb
John Jorgensen <john@leprechaun.dev>
2025-06-03 20:50:09 +0000

    move the type cause it was ambiguous and I got confused

--- packages/core/src/tartan-config.ts
+++ packages/core/src/tartan-config.ts
@@ -1,23 +1,6 @@
 import { TartanContextFile } from "./tartan-context.js";
 import { ReplaceTypes } from "./util.js";
 
-/**
- * Requiring pagemode and the relevant properties to be set (but we don't need to export this)
- */
-type FullTartanContext =
-    | ReplaceTypes<
-          TartanContextFile,
-          { pageMode: "file"; pageSource?: string; pagePattern: string }
-      >
-    | ReplaceTypes<
-          TartanContextFile,
-          { pageMode: "directory"; pageSource: string }
-      >
-    | ReplaceTypes<
-          TartanContextFile,
-          { pageMode: "asset"; pagePattern: string }
-      >;
-
 export type TartanConfig = {
     rootDir: string;
     outputDir: string;
@@ -29,5 +12,17 @@ export type TartanConfig = {
      * A list of regular expressions that match paths that'll be ignored.
      */
     ignoredPaths?: string[];
-    rootContext?: FullTartanContext;
+    rootContext?:
+        | ReplaceTypes<
+              TartanContextFile,
+              { pageMode: "file"; pageSource?: string; pagePattern: string }
+          >
+        | ReplaceTypes<
+              TartanContextFile,
+              { pageMode: "directory"; pageSource: string }
+          >
+        | ReplaceTypes<
+              TartanContextFile,
+              { pageMode: "asset"; pagePattern: string }
+          >; // because it's full context, but not initialized context
 };
