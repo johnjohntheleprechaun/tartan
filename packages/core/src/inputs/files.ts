d97e6b7c5b1a0a00f409562da9ece94a91a80db9
John Jorgensen <john@leprechaun.dev>
2025-09-03 21:21:30 -0600

    add module loader and file watching in src

---
+++ packages/core/src/inputs/files.ts
@@ -0,0 +1,49 @@
+import watcher from "@parcel/watcher";
+import path from "node:path";
+import { Subject } from "rxjs";
+
+export class FileWatcher {
+    private static subscription: watcher.AsyncSubscription; // I guess this isn't really used, but I'm keeping it in memory anyway idk
+    private static callbacks: watcher.SubscribeCallback[] = [];
+    public static addCallback(callback: watcher.SubscribeCallback) {
+        this.callbacks.push(callback);
+    }
+    private static callback(...params: Parameters<watcher.SubscribeCallback>) {
+        this.callbacks.forEach((callback) => callback(...params));
+    }
+    public static async dispose(): Promise<void> {
+        if (this.subscription) {
+            await this.subscription.unsubscribe();
+        }
+    }
+    static {
+        watcher
+            .subscribe(".", this.callback.bind(this), {
+                ignore: ["./node_modules/*"],
+            })
+            .then((sub) => (this.subscription = sub));
+    }
+
+    public watchedPaths: Set<string> = new Set<string>();
+    private subject: Subject<void>;
+    constructor(subscriber: Subject<void>) {
+        this.subject = subscriber;
+        FileWatcher.addCallback(this.localCallback.bind(this));
+    }
+    setWatchedPaths(paths: string[]) {
+        this.watchedPaths = new Set<string>(paths.map((a) => path.resolve(a)));
+    }
+    localCallback(err: Error | null, events: watcher.Event[]) {
+        /*
+        console.log("events triggered", events);
+        console.log("watched paths", this.watchedPaths);
+        */
+        for (const event of events) {
+            if (this.watchedPaths.has(event.path)) {
+                // just emit that something relevant happened
+                this.subject.next();
+                return;
+            }
+        }
+    }
+}
---
+++ packages/core/src/inputs/modules.ts
@@ -0,0 +1,141 @@
+import { createRequire } from "node:module";
+import rxjs, { filter, Observable, startWith, Subject, switchMap } from "rxjs"; // we need to import the entire rxjs lib so that we can provide it in the vm (so that things like instanceof will work across the boundary)
+import esbuild from "esbuild";
+import { FileWatcher } from "./files.js";
+import { Script } from "node:vm";
+
+const require = createRequire(import.meta.url);
+/**
+ * Get the default export of a module by building it with esbuild
+ * @param specifier The module to build
+ * @param relativeTo The path that specifier is relative to
+ */
+export function loadModule<T>(
+    specifier: string,
+    relativeTo?: string,
+): Observable<T> {
+    const exportSubject: Subject<T> = new Subject<T>();
+    const rebuildTrigger: Subject<void> = new Subject<void>();
+    const fileWatcher = new FileWatcher(rebuildTrigger);
+
+    // resolve specifier to a full path
+    const modulePath: string = require.resolve(specifier, {
+        paths: [relativeTo || process.cwd()],
+    });
+    // yes, this is a promise. it's supposed to be.
+    const buildContext = esbuild.context({
+        entryPoints: [modulePath],
+        format: "cjs",
+        platform: "node",
+        bundle: true,
+        write: false,
+        external: ["rxjs"],
+        metafile: true,
+        logLevel: "silent",
+    });
+
+    // The hash of the last outputted file, provided by esbuild
+    let lastBuildHash: string = "";
+    buildContext.then((ctx) => {
+        rebuildTrigger
+            .pipe(
+                startWith(undefined), // initial build
+                switchMap(() =>
+                    ctx
+                        .cancel()
+                        .then(() => ctx.rebuild())
+                        .then((result) => {
+                            if (result.warnings.length > 0) {
+                                const formattedWarnings =
+                                    esbuild.formatMessagesSync(
+                                        result.warnings,
+                                        {
+                                            kind: "warning",
+                                            color: true,
+                                        },
+                                    );
+                                // print logs
+                                console.log(
+                                    "==================================================\n",
+                                );
+                                console.log(
+                                    `Warnings while building ${modulePath}\n\n`,
+                                );
+                                console.log(formattedWarnings.join("\n"));
+                                console.log(
+                                    "==================================================",
+                                );
+                            }
+                            if (result.outputFiles.length !== 1) {
+                                throw `wrong number of output files. should be 1, was ${result.outputFiles.length}`;
+                            }
+
+                            const outputFile = result.outputFiles[0];
+                            if (outputFile.hash === lastBuildHash) {
+                                return undefined;
+                            }
+                            lastBuildHash = outputFile.hash;
+
+                            // set up file watching for the module and it's dependencies
+                            fileWatcher.setWatchedPaths(
+                                Object.keys(result.metafile.inputs),
+                            );
+
+                            /*
+                             * Run the script and extract the output
+                             */
+                            const script = new Script(outputFile.text, {
+                                filename: modulePath,
+                            });
+                            const context = {
+                                module: { exports: {} as any },
+                                require(path: string) {
+                                    return path === "rxjs" ? rxjs : null;
+                                },
+                            };
+
+                            script.runInNewContext(context);
+                            return context.module.exports.default;
+                        })
+                        .catch((reason) => {
+                            // if the build failed on the first go, make sure to watch the module path
+                            if (fileWatcher.watchedPaths.size === 0) {
+                                fileWatcher.setWatchedPaths([modulePath]);
+                            }
+                            // format logs
+                            const formattedErrors = esbuild.formatMessagesSync(
+                                reason.errors,
+                                {
+                                    kind: "error",
+                                    color: true,
+                                },
+                            );
+                            const formattedWarnings =
+                                esbuild.formatMessagesSync(reason.warnings, {
+                                    kind: "warning",
+                                    color: true,
+                                });
+
+                            // print logs
+                            console.log(
+                                "==================================================\n",
+                            );
+                            console.log(
+                                `Failed while building ${modulePath}\n\n`,
+                            );
+                            console.log(formattedErrors.join("\n"));
+                            formattedWarnings.length > 0
+                                ? console.log(formattedWarnings.join("\n"))
+                                : undefined;
+                            console.log(
+                                "==================================================",
+                            );
+                        }),
+                ),
+                filter((val) => val !== undefined),
+            )
+            .subscribe(exportSubject);
+    });
+
+    return exportSubject;
+}
