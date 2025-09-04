6f050c76961c2462c3ba38ac27a7603a30d06330
John Jorgensen <john@leprechaun.dev>
2025-09-03 21:20:36 -0600

    move old tests to an ignored directory

--- packages/core/spec/support/jasmine.ts
+++ packages/core/spec/support/jasmine.ts
@@ -1,6 +1,6 @@
 export default {
     spec_dir: "spec",
-    spec_files: ["**/*[sS]pec.ts"],
+    spec_files: ["**/*.spec.ts", "!old/**"],
     helpers: ["helpers/**/*.ts"],
     env: {
         stopSpecOnExpectationFailure: false,
