8309f7edc3255ef4475240bb1aa3d11c19b12f91
John Jorgensen <john@leprechaun.dev>
2025-10-01 22:53:53 -0600

    move logger to outputs dir

--- packages/core/src/outputs/logger.ts
+++ packages/core/src/outputs/logger.ts
@@ -1,5 +1,5 @@
 export class Logger {
-    public static defaultLogLevel = 0;
+    public static defaultLogLevel = 0; // no logs by default
     public static logLevel = this.defaultLogLevel;
     public static log(object: any, verbosity: number = 1) {
         if (verbosity <= this.logLevel) {
